8a2b34d4f0dd1167e9498c64ed1881c1a2c8f04e
Tom Bautz <tom.bautz@axelspringer.de>
2023-01-26 10:39:59 +0100

    add download functionality

--- src/__mocks__/webextension-polyfill-ts.ts
+++ src/__mocks__/webextension-polyfill-ts.ts
@@ -5,8 +5,8 @@
 
 import { setupGetMessageShim } from 'chrome-extension-i18n-shim';
 
-import messagesEN from '../static/_locales/de/messages.json';
-import messagesDE from '../static/_locales/en/messages.json';
+import messagesEN from '../static/_locales/en/messages.json';
+import messagesDE from '../static/_locales/de/messages.json';
 
 export const browser = {
   tabs: {
--- src/views/SaveBackupPhrase/SaveBackupPhrase.tsx
+++ src/views/SaveBackupPhrase/SaveBackupPhrase.tsx
@@ -14,10 +14,11 @@ export function SaveBackupPhrase({backupPhrase}: Props): JSX.Element {
   const words = backupPhrase.split(/\s+/);
   const t = browser.i18n.getMessage;
 
+  const orderedWordListForDownload = words.map((word, index) => `${index+1}: ${word}`).join("\n")
+
   return (
     <section className={styles.container}>
-      <h1 className={styles.heading}>{t('view_SaveBackupPhrase_explanation')}</h1>
-
+      <h1 className={styles.heading}>{t('view_SaveBackupPhrase_explanation')} <a download="as_wallet_words.txt" href={"data:text/plain;charset=utf-8,"+ encodeURIComponent(orderedWordListForDownload)}>{t('view_SaveBackupPhrase_download')}</a></h1>
       <ol className={styles.items}>
         {words.map((word, index) => (
           <li key={index} className={styles.item}>
